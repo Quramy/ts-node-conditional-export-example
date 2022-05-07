2c2e7afe191ae436f8c359d262b1cd9c9884d18f
Quramy <yosuke.kurami@gmail.com>
2022-05-07 22:02:29 +0900

    fix: Detect module kind using Jest configuration

--- packages/jest/src/index.ts
+++ packages/jest/src/index.ts
@@ -10,16 +10,25 @@ import type {
 class TsTransformer implements SyncTransformer {
   process(
     sourceText: string,
-    fileName: string,
-    _options: TransformOptions<unknown>
+    sourcePath: string,
+    { config: { extensionsToTreatAsEsm } }: TransformOptions<unknown>
   ): TransformedSource {
+    const shouldTreatDotTsFileAsEsm = extensionsToTreatAsEsm.includes(".ts");
+
+    // For now, transpileModule function cannot detect whether *.ts files to be treated as ESM.
+    // In Jest context, we can detect this via `extensionsToTreatAsEsm` property and control transpiled module kind switching fileName extension.
+    // See https://github.com/microsoft/TypeScript/issues/46452#issuecomment-1073154628
+    const fileName = sourcePath.endsWith(".ts")
+      ? sourcePath.replace(/\.ts$/, shouldTreatDotTsFileAsEsm ? ".mts" : ".cts")
+      : sourcePath;
+
     const transpileResult = ts.transpileModule(sourceText, {
       reportDiagnostics: false,
       fileName,
       compilerOptions: {
         target: ts.ScriptTarget.ES2022,
         module: ts.ModuleKind.NodeNext,
-        moduleDetection: ts.ModuleDetectionKind.Auto
+        moduleDetection: ts.ModuleDetectionKind.Force
       }
     });
     return {
